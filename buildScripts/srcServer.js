40981bf5d848179915b86f1c3f9a498df0943a1c
FarrukhKhalikoff <farrukhkhalikov@yahoo.com>
2018-06-03 20:30:52 -0400

    added babel

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -14,7 +14,7 @@ app.use(require('webpack-dev-middleware')(compiler, {
 }));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../srs/index.html'));
+  res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, function(err) {
